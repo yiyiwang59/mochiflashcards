c5e1ea84f064660f2e53da7031d239af850a5905
Yiyi Wang <yiyiwang@anthonys-imac.lan>
2024-08-07 23:21:54 -0400

    Turned airtable_API into Class and started writing unit test

--- python_scripts/airtable_API.py
+++ python_scripts/airtable_API.py
@@ -4,96 +4,102 @@ import os
 import chinese_dict_lookup as ch
 import requests
 
-# Load environment variables from .env file
-load_dotenv()
+class ConnectAirtableAPI:
+    def __init__(self):
+        # Load environment variables from .env file
+        load_dotenv()
 
-# Access the variables
-airtable_api_key = os.getenv('AIRTABLE_API_TOKEN')
-base_id = os.getenv('AIRTABLE_BASE_ID')
-vocab_table_id = os.getenv('AIRTABLE_VOCAB_TABLE_ID')
-lesson_table_id = os.getenv('AIRTABLE_LESSON_TABLE_ID')
-english_translation = os.getenv('AIRTABLE_ENGLISH_TRANSLATION_ID')
-english_translation_ref = formulas.FIELD(f'{english_translation}')
-ch_vocab_field = os.getenv('AIRTABLE_CH_VOCAB_ID')
-pinyin_field = os.getenv('AIRTABLE_PINYIN_ID')
-lesson_name_id = os.getenv('AIRTABLE_LESSON_NAME_ID')
-lesson_type_id = os.getenv('AIRTABLE_LESSON_TYPE_ID')
-mochi_deck_id = os.getenv('AIRTABLE_MOCHI_DECK_ID')
-mochi_deck_ref = formulas.FIELD(f'{mochi_deck_id}')
-mochi_card_id_ch = os.getenv('AIRTABLE_MOCHI_CARD_CH_ID')
-mochi_card_ref = formulas.FIELD(f'{mochi_card_id_ch}')
-mochi_card_id_eng = os.getenv('AIRTABLE_MOCHI_CARD_ENG_ID')
-
-
-api = Api(airtable_api_key)
-vocab_table = api.table(base_id, vocab_table_id)
-lesson_table = api.table(base_id, lesson_table_id)
-
-#Get any lines where english translation column is null
-def get_missing_translation_records():
-    airtable_json = vocab_table.all(fields=[f'{ch_vocab_field}'],formula=f"NOT({english_translation_ref})")
-    output = []
-    for record in airtable_json:
-        record_dict = {}
-        record_dict['id'] = record['id']
-        record_dict['vocab_word'] = record['fields']['Name'].strip()
-        output.append(record_dict)
-    return output
-
-#Fill in pinyin and english translation for all lines with english translation = null
-def fill_in_missing_data():
-    records = get_missing_translation_records()
-    complete_output = []
-    for line in records:
-        record_dict = {}
-        fields_dict = {}
-        pinyin_lookup, translation_lookup = ch.get_pinyin_translation(line['vocab_word'])
-        record_dict['id'] = line['id']
-        fields_dict[f'{ch_vocab_field}'] = line['vocab_word']
-        fields_dict[f'{pinyin_field}'] = pinyin_lookup
-        fields_dict[f'{english_translation}'] = translation_lookup
-        record_dict['fields'] = fields_dict
-        complete_output.append(record_dict)
-    vocab_table.batch_update(complete_output)
+        # Access the variables
+        self.airtable_api_key = os.getenv('AIRTABLE_API_TOKEN')
+        self.base_id = os.getenv('AIRTABLE_BASE_ID')
+        self.vocab_table_id = os.getenv('AIRTABLE_VOCAB_TABLE_ID')
+        self.lesson_table_id = os.getenv('AIRTABLE_LESSON_TABLE_ID')
+        self.english_translation = os.getenv('AIRTABLE_ENGLISH_TRANSLATION_ID')
+        self.ch_vocab_field = os.getenv('AIRTABLE_CH_VOCAB_ID')
+        self.pinyin_field = os.getenv('AIRTABLE_PINYIN_ID')
+        self.lesson_name_id = os.getenv('AIRTABLE_LESSON_NAME_ID')
+        self.lesson_type_id = os.getenv('AIRTABLE_LESSON_TYPE_ID')
+        self.mochi_deck_id = os.getenv('AIRTABLE_MOCHI_DECK_ID')
+        self.mochi_card_id_ch = os.getenv('AIRTABLE_MOCHI_CARD_CH_ID')
+        self.mochi_card_id_eng = os.getenv('AIRTABLE_MOCHI_CARD_ENG_ID')
 
 
-#get chinese lesson names with null mochi ID
-def get_decks_to_create():
-    airtable_json = lesson_table.all(fields=[f'{lesson_name_id}', f'{lesson_type_id}'],formula=f"NOT({mochi_deck_ref})")
-    output = []
-    for record in airtable_json:
-        record_dict = {}
-        record_dict['id'] = record['id']
-        record_dict['name'] = record['fields']['Name'].strip()
-        record_dict['type'] = record['fields']['Type']
-        output.append(record_dict)
-    return output
+        self.api = Api(self.airtable_api_key)
+        self.vocab_table = self.api.table(self.base_id, self.vocab_table_id)
+        self.lesson_table = self.api.table(self.base_id, self.lesson_table_id)
 
-#get chinese vocab data with null mochi ID
-def get_cards_to_create():
-    airtable_json = vocab_table.all(formula=f"NOT({mochi_card_ref})")
-    output = []
-    for record in airtable_json:
-        record_dict = {}
-        record_dict['id'] = record['id']
-        record_dict['vocab'] = record['fields']['Name'].strip()
-        record_dict['pinyin'] = record['fields']['Pinyin']
-        record_dict['english'] = record['fields']['English Translation']
-        record_dict['deck'] = get_lesson_name(record['fields']['Chinese Lesson'][0])
-        output.append(record_dict)
-    return output
-
-#populate mochi ID for chinese lesson
-def populate_mochi_id_lesson(lesson_id, mochi_id):
-    lesson_table.update(lesson_id, {f"{mochi_deck_id}": mochi_id})
+        self.english_translation_ref = formulas.FIELD(f'{self.english_translation}')
+        self.mochi_deck_ref = formulas.FIELD(f'{self.mochi_deck_id}')
+        self.mochi_card_ref = formulas.FIELD(f'{self.mochi_card_id_ch}')
 
 
 
-#populate mochi ID for chinese vocab
-def populate_mochi_id_vocab(vocab_id, mochi_id_ch, mochi_id_eng):
-    vocab_table.update(vocab_id, {f"{mochi_card_id_ch}": mochi_id_ch, f"{mochi_card_id_eng}": mochi_id_eng})
-    
-#lookup chinese lesson name from ID
-def get_lesson_name(lesson_id):
-    output = lesson_table.get(lesson_id)
-    return output['fields']['Name']
+
+    #Get any lines where english translation column is null
+    def get_missing_translation_records(self):
+        airtable_json = self.vocab_table.all(fields=[f'{self.ch_vocab_field}'],formula=f"NOT({self.english_translation_ref})")
+        output = []
+        for record in airtable_json:
+            record_dict = {}
+            record_dict['id'] = record['id']
+            record_dict['vocab_word'] = record['fields']['Name'].strip()
+            output.append(record_dict)
+        return output
+
+    #Fill in pinyin and english translation for all lines with english translation = null
+    def fill_in_missing_data(self):
+        records = self.get_missing_translation_records()
+        complete_output = []
+        for line in records:
+            record_dict = {}
+            fields_dict = {}
+            pinyin_lookup, translation_lookup = ch.get_pinyin_translation(line['vocab_word'])
+            record_dict['id'] = line['id']
+            fields_dict[f'{self.ch_vocab_field}'] = line['vocab_word']
+            fields_dict[f'{self.pinyin_field}'] = pinyin_lookup
+            fields_dict[f'{self.english_translation}'] = translation_lookup
+            record_dict['fields'] = fields_dict
+            complete_output.append(record_dict)
+        self.vocab_table.batch_update(complete_output)
+
+
+    #get chinese lesson names with null mochi ID
+    def get_decks_to_create(self):
+        airtable_json = self.lesson_table.all(fields=[f'{self.lesson_name_id}', f'{self.lesson_type_id}'],formula=f"NOT({self.mochi_deck_ref})")
+        output = []
+        for record in airtable_json:
+            record_dict = {}
+            record_dict['id'] = record['id']
+            record_dict['name'] = record['fields']['Name'].strip()
+            record_dict['type'] = record['fields']['Type']
+            output.append(record_dict)
+        return output
+
+    #get chinese vocab data with null mochi ID
+    def get_cards_to_create(self):
+        airtable_json = self.vocab_table.all(formula=f"NOT({self.mochi_card_ref})")
+        output = []
+        for record in airtable_json:
+            record_dict = {}
+            record_dict['id'] = record['id']
+            record_dict['vocab'] = record['fields']['Name'].strip()
+            record_dict['pinyin'] = record['fields']['Pinyin']
+            record_dict['english'] = record['fields']['English Translation']
+            record_dict['deck'] = self.get_lesson_name(record['fields']['Chinese Lesson'][0])
+            output.append(record_dict)
+        return output
+
+    #populate mochi ID for chinese lesson
+    def populate_mochi_id_lesson(self, lesson_id, mochi_id):
+        lesson_table.update(lesson_id, {f"{self.mochi_deck_id}": mochi_id})
+
+
+
+    #populate mochi ID for chinese vocab
+    def populate_mochi_id_vocab(self, vocab_id, mochi_id_ch, mochi_id_eng):
+        self.vocab_table.update(vocab_id, {f"{self.mochi_card_id_ch}": mochi_id_ch, f"{self.mochi_card_id_eng}": mochi_id_eng})
+        
+    #lookup chinese lesson name from ID
+    def get_lesson_name(self, lesson_id):
+        output = self.lesson_table.get(lesson_id)
+        return output['fields']['Name']
--- tests/airtable_API_test.py
+++ tests/airtable_API_test.py
@@ -1,3 +1,45 @@
+import unittest
+import pytest
+from unittest.mock import patch, MagicMock
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../python_scripts/')))
+
+from python_scripts.airtable_API import ConnectAirtableAPI
+
+class TestConnectAirtableAPI(unittest.TestCase):
+    @patch.dict(os.environ, {
+        'AIRTABLE_API_TOKEN': 'fake_api_token',
+        'AIRTABLE_BASE_ID': 'fake_base_id',
+        'AIRTABLE_VOCAB_TABLE_ID': 'fake_vocab_table_id',
+        'AIRTABLE_LESSON_TABLE_ID': 'fake_lesson_table_id',
+        'AIRTABLE_ENGLISH_TRANSLATION_ID': 'fake_english_translation_id',
+        'AIRTABLE_CH_VOCAB_ID': 'fake_ch_vocab_id',
+        'AIRTABLE_PINYIN_ID': 'fake_pinyin_id',
+        'AIRTABLE_LESSON_NAME_ID': 'fake_lesson_name_id',
+        'AIRTABLE_LESSON_TYPE_ID': 'fake_lesson_type_id',
+        'AIRTABLE_MOCHI_DECK_ID': 'fake_mochi_deck_id',
+        'AIRTABLE_MOCHI_CARD_CH_ID': 'fake_mochi_card_ch_id',
+        'AIRTABLE_MOCHI_CARD_ENG_ID': 'fake_mochi_card_eng_id'
+    })
+    def setUp(self):
+            self.manager = ConnectAirtableAPI()
+    
+    def test_get_missing_translation_records(self):
+        with patch.object(self.manager.vocab_table, 'all', return_value = [
+            {'id': 'rec1', 'fields': {'Name': '词1'}},
+            {'id': 'rec2', 'fields': {'Name': '词2'}}
+
+        ]):
+            records = self.manager.get_missing_translation_records()
+            self.assertEqual(len(records), 2)
+            self.assertEqual(records[0]['id'], 'rec1')
+            self.assertEqual(records[0]['vocab_word'], '词1')
+    
+    if __name__ == '__main__':
+         unittest.main()
+
 
 
         
